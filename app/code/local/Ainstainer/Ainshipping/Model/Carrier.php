886fcf831d4353b89a0095ccdcc80a9c1364f4fb
Serg <Serg@mail>
2016-01-05 20:34:45 +0200

    Add custom shipping method

---
+++ app/code/local/Ainstainer/Ainshipping/Model/Carrier.php
@@ -0,0 +1,54 @@
+<?php
+
+class Ainstainer_Ainshipping_Model_Carrier extends Mage_Shipping_Model_Carrier_Abstract
+    implements Mage_Shipping_Model_Carrier_Interface
+{
+    /**
+     * @var string
+     */
+    protected $_code = 'ainstainer_ainshipping';
+
+    /**
+     * @return array
+     */
+    public function getAllowedMethods()
+    {
+        return array(
+            'standard'    =>  'Standard delivery'
+        );
+    }
+
+    /**
+     * @param Mage_Shipping_Model_Rate_Request $request
+     * @return Mage_Shipping_Model_Rate_Result
+     */
+    public function collectRates(Mage_Shipping_Model_Rate_Request $request)
+    {
+        /** @var Mage_Shipping_Model_Rate_Result $result */
+        $result = Mage::getModel('shipping/rate_result');
+
+        $result->append($this->_getStandardRate());
+
+        return $result;
+    }
+
+    /**
+     * Get Standard rate object
+     *
+     * @return Mage_Shipping_Model_Rate_Result_Method
+     */
+    protected function _getStandardRate()
+    {
+        /** @var Mage_Shipping_Model_Rate_Result_Method $rate */
+        $rate = Mage::getModel('shipping/rate_result_method');
+
+        $rate->setCarrier($this->_code);
+        $rate->setCarrierTitle($this->getConfigData('title'));
+        $rate->setMethod($this->_code);
+        $rate->setMethodTitle($this->getConfigData('name'));
+        $rate->setPrice($this->getConfigData('price'));
+        $rate->setCost(0);
+
+        return $rate;
+    }
+}
